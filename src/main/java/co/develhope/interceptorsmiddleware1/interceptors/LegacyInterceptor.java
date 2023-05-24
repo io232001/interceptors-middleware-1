2ccd5dd23bfe3116acede227c98941414e7da254
Ionut Mazilu <io232001@gmail.com>
2023-05-24 13:27:53 +0200

    Initial commit

---
+++ src/main/java/co/develhope/interceptorsmiddleware1/interceptors/LegacyInterceptor.java
@@ -0,0 +1,22 @@
+package co.develhope.interceptorsmiddleware1.interceptors;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+@Component
+public class LegacyInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getRequestURL().toString().contains("/legacy")){
+            response.setStatus(404);
+            throw new Exception();
+        }
+        return true;
+    }
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
